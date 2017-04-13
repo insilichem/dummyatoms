7bceead8f75fca7bf57c72baa513a99cdeb31e04
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2017-04-13 17:50:06 +0200

    test_build_dummies incorporated

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -27,7 +27,7 @@ import pytest
 from conftest import metal_atom, datapath, search_for_metal
 
 
-@pytest.mark.parametrize("charge", "geom", "dummies_xyz" [
+@pytest.mark.parametrize("charge, geom, dummies_xyz", [
     (2, "tetrahedral", [[28.299, 41.696, 30.738], [29.650, 41.653, 30.161], [28.485, 41.307, 29.333], [28.916, 40.404, 30.409]])
 ])
 def test_build_dummies(charge, geom, dummies_xyz):
@@ -43,15 +43,15 @@ def test_build_dummies(charge, geom, dummies_xyz):
                       'SQUARE_PYRAMID': [charge / 5.0 for i in range(0, 5)],
                       'OCTAHEDRON': [charge / 6.0 for i in range(0, 6)]
                       }
-    # get metal
+    # get metal chimera object
     path = datapath('zinc.pdb')
     metal = search_for_metal(path, 'zn')
-    # Produce dummy instances as a metal attribute
-    metal_atom(metal, charge, geom)
-    metal.build_dummies(dummies_xyz, geom, charge)
+    # Produce dummy&metal instances from chimera object
+    metal_class = metal_atom(metal, charge, geom)
+    metal_class.build_dummies(dummies_xyz, geom, charge)
     # Evaluation
-    for i in range(0, len(dummies_xyz)):
-        dummy = getattr(metal, "D{}".format(i))
+    for i in range(1, len(dummies_xyz)):
+        dummy = getattr(metal_class, "D{}".format(i))
         assert dummy.xyz == dummies_xyz[i - 1]
-        assert dummy.Type == dummies_type[geom.upper][i - 1]
-        assert dummy.charge == dummies_charge[geom.upper][i - 1]
+        assert dummy.Type == dummies_type[geom.upper()][i - 1]
+        assert dummy.charge == dummies_charge[geom.upper()][i - 1]
